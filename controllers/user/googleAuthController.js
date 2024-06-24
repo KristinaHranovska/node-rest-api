299f0bcdd725346b21ea6fea3d8e572046818a7d
Kristina <kr.hranovska@gmail.com>
2024-06-24 17:29:05 +0300

    Update googleAuthController.js

--- controllers/user/googleAuthController.js
+++ controllers/user/googleAuthController.js
@@ -68,5 +68,5 @@ export const googleRedirect = async (req, res) => {
 
     await User.findByIdAndUpdate(user._id, { token, refreshToken });
 
-    return res.redirect(`${FRONTEND_URL}/tracker/google-redirect/?token=${token}&refreshToken=${refreshToken}`);
+    return res.redirect(`${FRONTEND_URL}/tracker`);
 }

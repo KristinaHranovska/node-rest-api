7eaaad28841a1f2dd07627f6fa356907069a530c
Kristina <kr.hranovska@gmail.com>
2024-06-25 21:14:40 +0300

    Update verifyEmail.js

--- controllers/user/verifyEmail.js
+++ controllers/user/verifyEmail.js
@@ -7,7 +7,7 @@ const { SECRET_KEY, FRONTEND_URL } = process.env;
 
 export const verifyEmail = async (req, res, next) => {
     try {
-        const { token: verificationToken } = req.params;
+        const { verificationToken } = req.params;
         const user = await User.findOne({ verificationToken });
 
         if (!user) {
@@ -15,19 +15,19 @@ export const verifyEmail = async (req, res, next) => {
         }
 
         if (user.isVerified) {
-            const newToken = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
-            return res.redirect(`${FRONTEND_URL}/verify-email?token=${newToken}`);
+            jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
+            return res.redirect(`${FRONTEND_URL}/verify-email?token=${token}`);
         }
 
         user.isVerified = true;
         await user.save();
 
         const payload = { id: user._id };
-        const newToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
+        const tokenNew = jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
 
-        await User.findByIdAndUpdate(user._id, { token: newToken });
+        await User.findByIdAndUpdate(user._id, { token: tokenNew });
 
-        return res.redirect(`${FRONTEND_URL}/verify-email?token=${newToken}`);
+        return res.redirect(`${FRONTEND_URL}/verify-email?token=${tokenNew}`);
     } catch (error) {
         next(error);
     }
